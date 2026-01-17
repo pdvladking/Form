1907348b1f2f96c21c20305008c334790001e121
pdvladking <pdvladking@gmail.com>
2026-01-17 13:08:45 +0545

    connecting render-1

--- server/routes/formRoutes.js
+++ server/routes/formRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { submitForm } = require('../controllers/formController');
 
-//POST /api/form/submit
+// POST /api/form/submit
 router.post('/submit', submitForm);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const formRoutes = require('./routes/formRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// MONGODB
+// Connect to MongoDB
 connectDB();
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); // built-in JSON parser
 
 // Routes
-app.use('api/form', formRoutes);
+app.use('/api/form', formRoutes);
 
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
