411800a64cf2673508416d2835c852de322989f7
pdvladking <pdvladking@gmail.com>
2026-01-18 10:37:59 +0545

    fix: render deployment

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
-import formRoutes from "./routes/formRoutes";
+import formRoutes from "./routes/formRoutes.js";
 
 dotenv.config();
 const app = express();
